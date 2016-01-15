4536cc60aa4b112fc3038ecafd0f135494400692
antonioortegajr <antonioortegajr@gmail.com>
2016-01-15 08:57:12 -0800

    specified json
    added request header to only return son since zapper don’t do the xml
    so much.

--- idxbroker_zap.py
+++ idxbroker_zap.py
@@ -4,7 +4,7 @@ import json
 
 #set endpoint and add your API key
 url = "https://api.idxbroker.com/leads/lead?interval=.25&dateType=subscribeDate"
-headers = {"Content-Type":"application/x-www-form-urlencoded", "accesskey":"YourAPIKeyHere"}
+headers = {"Content-Type":"application/x-www-form-urlencoded", "accesskey":"YourAPIKeyHere","outputtype":"json"}
 domain = urlparse.urlparse(url).netloc
 connection = httplib.HTTPSConnection(domain)
 connection.request("GET", url, headers=headers)
